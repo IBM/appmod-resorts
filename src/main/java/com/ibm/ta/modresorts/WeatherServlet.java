71a7f229170e76c53a6e5fca3dbe2fb893325b1d
Mark Sturdevant <mark.sturdevant@ibm.com>
2018-09-21 13:04:20 -0700

    Code formatter and format fixes

--- src/main/java/com/ibm/ta/modresorts/WeatherServlet.java
+++ src/main/java/com/ibm/ta/modresorts/WeatherServlet.java
@@ -1,7 +1,7 @@
 package com.ibm.ta.modresorts;
 
+import com.ibm.ta.modresorts.exception.ExceptionHandler;
 import java.io.BufferedReader;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -12,181 +12,180 @@ import java.util.Arrays;
 import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-import com.ibm.ta.modresorts.exception.ExceptionHandler;
-
 public class WeatherServlet extends HttpServlet {
 
+  // local OS environment variable key name.  The key value should provide an API key that will be
+  // used to
+  // get weather information from site: http://www.wunderground.com
+  private static final String WEATHER_API_KEY = "WEATHER_API_KEY";
 
-	// local OS environment variable key name.  The key value should provide an API key that will be used to
-	// get weather information from site: http://www.wunderground.com
-	private static final String WEATHER_API_KEY = "WEATHER_API_KEY";  
-	
-	static final long serialVersionUID = 1L;
-	  
-	private static final Logger logger = Logger.getLogger(WeatherServlet.class.getName());
+  static final long serialVersionUID = 1L;
 
-	/**
-	 * constructor
-	 */
-	public WeatherServlet() {
-		super();
-	}
+  private static final Logger logger = Logger.getLogger(WeatherServlet.class.getName());
 
-	/**
-	 * Returns the weather information for a given city
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException{
-		String methodName = "doGet";
-		logger.entering(WeatherServlet.class.getName(), methodName);
+  /** constructor */
+  public WeatherServlet() {
+    super();
+  }
 
-		String city = request.getParameter("selectedCity");
-		logger.log(Level.FINE, "requested city is " + city);
-		
-		String weatherAPIKey = System.getenv(WEATHER_API_KEY);
-		String mockedKey = mockKey(weatherAPIKey);
-		logger.log(Level.FINE, "weatherAPIKey is " + mockedKey);
-		
-		if (weatherAPIKey != null && weatherAPIKey.trim().length() > 0) {
-			logger.info("weatherAPIKey is found, system will provide the real time weather data for the city " + city);
-			getRealTimeWeatherData(city, weatherAPIKey, response);
-		}else {
-			logger.info("weatherAPIKey is not found, will provide the weather data dated August 10th, 2018 for the city " + city);
-			getDefaultWeatherData(city, response);
-		}
-	}
-	
-	private void getRealTimeWeatherData(String city, String apiKey, HttpServletResponse response) 
-			throws ServletException, IOException {
-		String resturl = null;
-		String resturlbase = Constants.WUNDERGROUND_API_PREFIX + apiKey + Constants.WUNDERGROUND_API_PART;
+  /** Returns the weather information for a given city */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String methodName = "doGet";
+    logger.entering(WeatherServlet.class.getName(), methodName);
 
-	    if (Constants.PARIS.equals(city)) {
-	            resturl = resturlbase + "France/Paris.json";
-	        } else if (Constants.LAS_VEGAS.equals(city)) {
-	            resturl = resturlbase + "NV/Las_Vegas.json";
-	        } else if (Constants.SAN_FRANCISCO.equals(city)) {
-	            resturl = resturlbase + "/CA/San_Francisco.json";
-	        } else if (Constants.MIAMI.equals(city)) {
-	            resturl = resturlbase + "FL/Miami.json";
-	        } else if (Constants.CORK.equals(city)) {
-	            resturl = resturlbase + "ireland/cork.json";
-	        } else if (Constants.BARCELONA.equals(city)) {
-	            resturl = resturlbase + "Spain/Barcelona.json";
-		}else {			
-			String errorMsg = "Sorry, the weather information for your selected city: " + city + 
-					" is not available.  Valid selections are: " + Arrays.toString(Constants.SUPPORTED_CITIES);
-			ExceptionHandler.handleException(null, errorMsg, logger);
-		}
-			
-		URL obj;
-		HttpURLConnection con = null;
-		try {
-			obj = new URL(Objects.requireNonNull(resturl));
-			con = (HttpURLConnection) obj.openConnection();
-			con.setRequestMethod("GET");
-		} catch (MalformedURLException e1) {
-			String errorMsg = "Caught MalformedURLException. Please make sure the url is correct.";
-			ExceptionHandler.handleException(e1, errorMsg, logger);
-		}catch (ProtocolException e2) {
-			String errorMsg = "Caught ProtocolException: " + e2.getMessage() + ". Not able to set request method to http connection.";
-			ExceptionHandler.handleException(e2, errorMsg, logger);
-		} catch (IOException e3) {
-			String errorMsg = "Caught IOException: " + e3.getMessage() + ". Not able to open connection.";
-			ExceptionHandler.handleException(e3, errorMsg, logger);
-		} 
-		
-		int responseCode = Objects.requireNonNull(con).getResponseCode();
-		logger.log(Level.FINEST, "Response Code: " + responseCode);		
-		
-		if (responseCode >= 200 && responseCode < 300) {
+    String city = request.getParameter("selectedCity");
+    logger.log(Level.FINE, "requested city is " + city);
 
-			BufferedReader in = null;
-			ServletOutputStream out = null;
+    String weatherAPIKey = System.getenv(WEATHER_API_KEY);
+    String mockedKey = mockKey(weatherAPIKey);
+    logger.log(Level.FINE, "weatherAPIKey is " + mockedKey);
 
-			try {
-				in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-				String inputLine;
-				StringBuilder responseStr = new StringBuilder();
-				
-				while ((inputLine = in.readLine()) != null) {
-					responseStr.append(inputLine);
-				}
-	
-				response.setContentType("application/json");
-				out = response.getOutputStream();
-				out.print(responseStr.toString());
-				logger.log(Level.FINE, "responseStr: " + responseStr);
-			} catch (Exception e) {
-				String errorMsg = "Problem occured when processing the weather server response.";
-				ExceptionHandler.handleException(e, errorMsg, logger);
-			} finally {
-				if (in != null) {
-					in.close();
-				}
-				if (out!= null) {
-					out.close();
-				}
-            }
-		} else {
-			String errorMsg = "REST API call " + resturl + " returns an error response: " + responseCode;
-			ExceptionHandler.handleException(null, errorMsg, logger);
-		}
-	}
-	
-	private void getDefaultWeatherData(String city, HttpServletResponse response) 
-			throws ServletException, IOException {
-		DefaultWeatherData defaultWeatherData = null;
-	
-		try {
-			defaultWeatherData = new DefaultWeatherData(city);
-		}catch (UnsupportedOperationException e) {
-			ExceptionHandler.handleException(e, e.getMessage(), logger);
-		}
-		
-		ServletOutputStream out = null;
-		
-		try {
-			String responseStr = Objects.requireNonNull(defaultWeatherData).getDefaultWeatherData();
-			response.setContentType("application/json");
-			out = response.getOutputStream();
-			out.print(responseStr);
-			logger.log(Level.FINEST, "responseStr: " + responseStr);
-		} catch (Exception e) {
-				String errorMsg = "Problem occured when getting the default weather data.";
-				ExceptionHandler.handleException(e, errorMsg, logger);
-		} finally {
-				
-			if (out!= null) {
-				out.close();
-			}
+    if (weatherAPIKey != null && weatherAPIKey.trim().length() > 0) {
+      logger.info(
+          "weatherAPIKey is found, system will provide the real time weather data for the city "
+              + city);
+      getRealTimeWeatherData(city, weatherAPIKey, response);
+    } else {
+      logger.info(
+          "weatherAPIKey is not found, will provide the weather data dated August 10th, 2018 for the city "
+              + city);
+      getDefaultWeatherData(city, response);
+    }
+  }
 
+  private void getRealTimeWeatherData(String city, String apiKey, HttpServletResponse response)
+      throws ServletException, IOException {
+    String resturl = null;
+    String resturlbase =
+        Constants.WUNDERGROUND_API_PREFIX + apiKey + Constants.WUNDERGROUND_API_PART;
+
+    if (Constants.PARIS.equals(city)) {
+      resturl = resturlbase + "France/Paris.json";
+    } else if (Constants.LAS_VEGAS.equals(city)) {
+      resturl = resturlbase + "NV/Las_Vegas.json";
+    } else if (Constants.SAN_FRANCISCO.equals(city)) {
+      resturl = resturlbase + "/CA/San_Francisco.json";
+    } else if (Constants.MIAMI.equals(city)) {
+      resturl = resturlbase + "FL/Miami.json";
+    } else if (Constants.CORK.equals(city)) {
+      resturl = resturlbase + "ireland/cork.json";
+    } else if (Constants.BARCELONA.equals(city)) {
+      resturl = resturlbase + "Spain/Barcelona.json";
+    } else {
+      String errorMsg =
+          "Sorry, the weather information for your selected city: "
+              + city
+              + " is not available.  Valid selections are: "
+              + Arrays.toString(Constants.SUPPORTED_CITIES);
+      ExceptionHandler.handleException(null, errorMsg, logger);
+    }
+
+    URL obj;
+    HttpURLConnection con = null;
+    try {
+      obj = new URL(Objects.requireNonNull(resturl));
+      con = (HttpURLConnection) obj.openConnection();
+      con.setRequestMethod("GET");
+    } catch (MalformedURLException e1) {
+      String errorMsg = "Caught MalformedURLException. Please make sure the url is correct.";
+      ExceptionHandler.handleException(e1, errorMsg, logger);
+    } catch (ProtocolException e2) {
+      String errorMsg =
+          "Caught ProtocolException: "
+              + e2.getMessage()
+              + ". Not able to set request method to http connection.";
+      ExceptionHandler.handleException(e2, errorMsg, logger);
+    } catch (IOException e3) {
+      String errorMsg = "Caught IOException: " + e3.getMessage() + ". Not able to open connection.";
+      ExceptionHandler.handleException(e3, errorMsg, logger);
+    }
+
+    int responseCode = Objects.requireNonNull(con).getResponseCode();
+    logger.log(Level.FINEST, "Response Code: " + responseCode);
+
+    if (responseCode >= 200 && responseCode < 300) {
+
+      BufferedReader in = null;
+      ServletOutputStream out = null;
+
+      try {
+        in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder responseStr = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+          responseStr.append(inputLine);
         }
-	}
 
-	/**
-	 * Returns the weather information for a given city
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+        response.setContentType("application/json");
+        out = response.getOutputStream();
+        out.print(responseStr.toString());
+        logger.log(Level.FINE, "responseStr: " + responseStr);
+      } catch (Exception e) {
+        String errorMsg = "Problem occured when processing the weather server response.";
+        ExceptionHandler.handleException(e, errorMsg, logger);
+      } finally {
+        if (in != null) {
+          in.close();
+        }
+        if (out != null) {
+          out.close();
+        }
+      }
+    } else {
+      String errorMsg = "REST API call " + resturl + " returns an error response: " + responseCode;
+      ExceptionHandler.handleException(null, errorMsg, logger);
+    }
+  }
 
-		doGet(request, response);
-	}	
-	
-	private static String mockKey(String toBeMocked) {
-		if (toBeMocked == null) {
-			return null;
-		}
-		String lastToKeep = toBeMocked.substring(toBeMocked.length()-3);
-		return "*********" + lastToKeep;
-	}
-	
+  private void getDefaultWeatherData(String city, HttpServletResponse response)
+      throws ServletException, IOException {
+    DefaultWeatherData defaultWeatherData = null;
+
+    try {
+      defaultWeatherData = new DefaultWeatherData(city);
+    } catch (UnsupportedOperationException e) {
+      ExceptionHandler.handleException(e, e.getMessage(), logger);
+    }
+
+    ServletOutputStream out = null;
+
+    try {
+      String responseStr = Objects.requireNonNull(defaultWeatherData).getDefaultWeatherData();
+      response.setContentType("application/json");
+      out = response.getOutputStream();
+      out.print(responseStr);
+      logger.log(Level.FINEST, "responseStr: " + responseStr);
+    } catch (Exception e) {
+      String errorMsg = "Problem occured when getting the default weather data.";
+      ExceptionHandler.handleException(e, errorMsg, logger);
+    } finally {
+
+      if (out != null) {
+        out.close();
+      }
+    }
+  }
+
+  /** Returns the weather information for a given city */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    doGet(request, response);
+  }
+
+  private static String mockKey(String toBeMocked) {
+    if (toBeMocked == null) {
+      return null;
+    }
+    String lastToKeep = toBeMocked.substring(toBeMocked.length() - 3);
+    return "*********" + lastToKeep;
+  }
 }
--- src/main/java/com/ibm/ta/modresorts/exception/ExceptionHandler.java
+++ src/main/java/com/ibm/ta/modresorts/exception/ExceptionHandler.java
@@ -2,19 +2,18 @@ package com.ibm.ta.modresorts.exception;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.servlet.ServletException;
 
 public class ExceptionHandler {
-	
-	public static void handleException(Exception e, String errorMsg, Logger logger) throws ServletException {
-		if (e == null) {
-			logger.severe(errorMsg);
-			throw new ServletException(errorMsg);
-		}else {
-			logger.log(Level.SEVERE, errorMsg, e);
-			throw new ServletException(errorMsg, e);
-		}
-	}
-}
 
+  public static void handleException(Exception e, String errorMsg, Logger logger)
+      throws ServletException {
+    if (e == null) {
+      logger.severe(errorMsg);
+      throw new ServletException(errorMsg);
+    } else {
+      logger.log(Level.SEVERE, errorMsg, e);
+      throw new ServletException(errorMsg, e);
+    }
+  }
+}
